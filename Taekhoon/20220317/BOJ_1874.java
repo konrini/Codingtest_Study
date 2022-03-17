296a281033bf0a4f6db86e33483a81cc9da72fb3
양택훈 <97661931+korHoonie@users.noreply.github.com>
2022-03-17 16:23:13 +0900

    upload 20220317 HW
    BOJ_1874 - 스택 수열
    BOJ_1929 - 소수 구하기
    BOJ_1966 - 프린터 큐

---
+++ Taekhoon/20220317/BOJ_1874.java
@@ -0,0 +1,20 @@
+package boj.class2;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class BOJ_1874 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Stack<Integer> s = new Stack<>();
+		int curr = sc.nextInt();
+		int inputNum = 0;
+
+		while(n-- > 0) {
+			
+		}
+
+	}
+}
